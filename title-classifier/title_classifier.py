8f47fb540dccef0a65599d1404cd99adbec9512b
18adrianoh <adrianohernandez2000gmail.com>
2020-09-07 16:57:01 -0700

    can download images :)

--- title-classifier/title_classifier.py
+++ title-classifier/title_classifier.py
@@ -1,6 +1,11 @@
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 
+# from youtube thumbnail/image download tutorial
+import requests # to get image from the web
+import shutil # to save it locally
+
+
 """ assumes valid format per the v1-data.txt """
 def parse_file(filename):
     titles = []
@@ -30,7 +35,7 @@ def sieve_languages(titles):
 
     return langs
 
-PATH = "./classes/"
+CLASSES_PATH = "./classes/"
 
 class_files = {
     "misc" : "miscellaneous.txt",
@@ -38,7 +43,7 @@ class_files = {
 }
 
 class_titles = {
-    _class : parse_file(PATH + class_file) for _class, class_file in class_files.items()
+    _class : parse_file(CLASSES_PATH + class_file) for _class, class_file in class_files.items()
 }
 
 languages = {
@@ -55,3 +60,28 @@ tests = [
     "Lovesickness [NSF, 2a03]",
     "【東方ボーカル】 「螺旋絶望」 【豚乙女】"
 ]
+
+# try get thumbnail to do CV on it
+# from http://www.youtube.com/watch?v=VIDEOID
+# to http://img.youtube.com/vi/VIDEOID/#.jpg
+def download_image(url, path):
+    # WORKS! will get connection refused, however, if you have blockers on
+    #url_original = "http://www.youtube.com/watch?v=LFbhGEiFWk4"
+    # url = "https://img.youtube.com/vi/LFbhGEiFWk4/0.jpg" 
+    filename = path + url.split("/")[-1]
+
+    # Open the url image, set stream to True, this will return the stream content.
+    r = requests.get(url, stream = True)
+
+    # Check if the image was retrieved successfully
+    if r.status_code == 200:
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        r.raw.decode_content = True
+        
+        # Open a local file with wb ( write binary ) permission.
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            
+        return filename
+    else:
+        return None
