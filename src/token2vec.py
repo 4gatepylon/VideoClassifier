3040e395d88203c9a1aff2766b64226efb588993
18adrianoh <adrianohernandez2000gmail.com>
2020-10-05 10:02:57 -0700

    untested cbow completed

--- src/token2vec.py
+++ src/token2vec.py
@@ -94,6 +94,41 @@ class NGramLanguageModeler(nn.Module):
     def embedding_for(self, token, transform=None):
         return self.embeddings(token if transform is None else transform(token))
 
+class CBOW(nn.Module):
+
+    # cbow_size includes middle to context size is cbow_size // 2 on each side (it's odd)
+    def __init__(self, vocab_size, embedding_dim, cbow_size):
+        super(CBOW, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.linear1 = nn.Linear(2 * (cbow_size - 1) * embedding_dim, 264)
+        self.linear2 = nn.Linear(264, vocab_size)
+
+    def forward(self, inputs):
+        embeds = self.embeddings(inputs).view((1, -1))
+        out = F.relu(self.linear1(embeds))
+        out = self.linear2(out)
+        log_probs = F.log_softmax(out, dim=1)
+        return log_probs
+    
+    def embedding_dictionary(self, tokens, transform=None, key_transform=False):
+        if key_transform:
+            return (
+                {
+                    token: self.embeddings(
+                        token if transform is None else transform(token)
+                    )
+                    for token in tokens
+                },
+                None,
+            )
+        else:
+            return {
+                token: self.embeddings(token)
+                for token in map(lambda token: transform(token), tokens)
+            }
+
+    def embedding_for(self, token, transform=None):
+        return self.embeddings(token if transform is None else transform(token))
 
 """
 here we have some file helpers to write embeddings and other useful information like
@@ -169,9 +204,78 @@ def create_embeddings_cbow(
     lr=LEARNING_RATE_CBOW,
     use_pretrained=True,
 ):
-    # TODO CBOW
-    return None, None, None
+    cbows = _map_class2tokens_to_cbows(class2tokens) # TODO
 
+    _vocab = _generate_vocab(cbows) # this can be shared by both cbows and ngrams
+    vocab_size = len(_vocab)
+
+    token2ix = {word: i for i, word in enumerate(vocab)}
+
+    if CBOW_SIZE % 2 == 0:
+        raise ValueError("Cannot have even CBOW_SIZE... it has to be odd so that the left and right are each equal with the target in the middle.")
+    
+    if use_pretrained:
+        try:
+            model = load_model(path)
+        except:
+            print("Failed to load cbow model, creating a new one")
+            model = CBOW(vocab_size, EMBEDDING_DIM, CBOW_SIZE)
+    else:
+        model = CBOW(vocab_size, EMBEDDING_DIM, CBOW_SIZE)
+
+    loss_function = nn.NLLLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.005)
+
+    print(f"training n-gram for {_type}...")
+    for epoch in range(epochs):
+        print(f"\tepoch {epoch}")
+
+        total_loss = 0
+        for cbow in some_items(batch_size, cbows):
+            target = cbow[len(cbow) // 2] # i.e. 1 2 3 4 5 -> want 3 5 // 2 = 2 wich is right in zero indexing
+            context = _cbow_context(cbow)
+
+            context_idxs = torch.tensor([token2ix[w] for w in context], dtype=torch.long)
+
+            model.zero_grad()
+            log_probs = model(context_idxs)
+
+            loss = loss_function(log_probs, torch.tensor([token2ix[target]], dtype=torch.long))
+
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+    print("finished training n-gram based token2vec (from the batch size)")
+    for i in range(len(losses)):
+        print(f"\tloss in epoch {i} was {losses[i]}")
+
+    save_model(model, path)
+
+    _transform = lambda token, token2ix: torch.tensor(
+        [token2ix[token]], dtype=torch.long
+    )
+
+    embeddings = model.embedding_dictionary(
+        vocab, transform=lambda token: _transform(token, token2ix)
+    )
+
+    return embeddings, _transform, token2ix
+
+def _map_class2tokens_to_cbows(class2tokens):
+    # this is possible since we don't store a differen't format, we just query it differently later
+    return _map_class2tokens_to_n_grams(class2tokens)
+
+def _cbow_context(cbow, as_list=True):
+    generator = _cbow_context_generator(cbow)
+    return list(generator) if as_list else generator
+
+def _cbow_context_generator(cbow):
+    for i in range(0, len(cbow) // 2):
+        yield cbow[i]
+    # ignore the target (len(cbow) // 2) precisely
+    for j in range(len(cbow) // 2 + 1, len(cbow)):
+        yield cbow[i]
 
 """ below is all the n-gram code (older) """
 
